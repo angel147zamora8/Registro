691a61cc8012bea01ecfb2775d1068ff37f903a3
angel147zamora8 <azamoran123456789@gmail.com>
2021-05-13 15:25:40 -0500

    se le actualizo el diseño

---
+++ Registro/index.js
@@ -0,0 +1,12 @@
+function Guardar() {
+    const phonen = parseInt(document.getElementById("phonen").Value);
+    const namen = document.getElementById("namen").Value;
+    const years = parseInt(document.getElementById("years").Value);
+    
+    let x =[phonen,namen,years];
+    return x;
+}
+
+
+
+
